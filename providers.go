c6556a7884ba02fc42c21607ab7810c87ef9250d
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2015-11-02 16:19:55 -0500

    Implement Exists() method

---
+++ providers.go
@@ -0,0 +1,27 @@
+package emailproviders
+
+import (
+	"log"
+	"strings"
+)
+
+func Exists(email string) bool {
+	log.Printf("exists? %s", email)
+	at := strings.LastIndexByte(email, '@')
+	if at <= 0 || len(email[at:]) < 3 {
+		log.Printf("false")
+		return false
+	}
+	log.Printf("exists? %s", email[at+1:])
+
+	_, exists := publicProviders[email[at+1:]]
+
+	log.Print(exists)
+
+	return exists
+}
+
+var publicProviders = map[string]struct{}{
+	//go:generate go run generate.go
+	"gmail.com": struct{}{},
+}
