17c13be1b0451242419b573b2c6811d0743b22c3
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-11-02 18:17:13 -0500

    go 1.4

--- providers.go
+++ providers.go
@@ -1,14 +1,11 @@
 package emailproviders
 
-import (
-	"log"
-	"strings"
-)
+import "log"
 
 //go:generate go run generate/main.go generate/domains.txt domains.go
 
 func Exists(email string) bool {
-	at := strings.LastIndexByte(email, '@')
+	at := lastIndexByte(email, '@')
 	if at <= 0 || len(email[at:]) < 3 {
 		log.Printf("false")
 		return false
@@ -16,4 +13,14 @@ func Exists(email string) bool {
 
 	_, exists := publicProviders[email[at+1:]]
 	return exists
+}
+
+// lastIndexByte returns the index of the last instance of c in s, or -1 if c is not present in s.
+func lastIndexByte(s string, c byte) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == c {
+			return i
+		}
+	}
+	return -1
 }
